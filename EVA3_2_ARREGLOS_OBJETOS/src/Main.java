cfddaec1f38def40796eca5c02762a2acbc3f5d2
Irving4326 <43560217+Irving4326@users.noreply.github.com>
2018-12-01 10:09:10 -0700

    Update EVA3_2_ARREGLOS_OBJETOS/src/Main.java

---
+++ EVA3_2_ARREGLOS_OBJETOS/src/Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Irving Gerardo Mancera Mejia
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona[] apAlumnos = new Persona[5];
+        for (int i = 0; i < apAlumnos.length; i++) {
+            apAlumnos[i]  = new Persona("Juan Perez");
+        }
+        for (int i = 0; i < apAlumnos.length; i++) {
+            apAlumnos[i].imprimir();
+        }
+    }
+    
+}
+class Persona{
+public String nombre;
+public void imprimir(){
+            System.out.println(nombre);
+}
+
+
+public Persona(String nombre){
+this.nombre = nombre;
+}
+}
